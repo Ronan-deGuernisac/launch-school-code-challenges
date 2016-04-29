b82ff32e7b8163a7a8c4a35f5b2a09bbf2ce9163
superchilled <karl@superchilled.co.uk>
2016-04-29 17:20:35 +0000

    Refactor to improve code structure and syntax

--- series/series.rb
+++ series/series.rb
@@ -2,28 +2,37 @@
 
 # 1. transform the string to an array
 # 2. calculate how many series there will be (e.g length minus n + 1)
-# 3. create the sub-arrays in a loop and push tehm to the main array
+# 3. create the sub-arrays in a loop and push them to the main array
 
-
-class Series
+class Series # :nodoc:
   def initialize(string)
-    @string = string
+    @number_array = to_num_array(string)
+    @length = @number_array.length
   end
-  
+
   def slices(n)
-    num_arr = @string.split('').map { |idx| idx.to_i }
-    len = num_arr.length
+    if n > @length
+      raise ArgumentError, "Requested series of length #{n} but string only of
+      length #{@length}"
+    else
+      outer_array = sub_array_loop(n)
+    end
+    outer_array
+  end
+
+  private
+
+  def sub_array_loop(n)
     outer_arr = []
     counter = 0
-    if n > len
-      raise ArgumentError#, 'Can only add Todo objects'
-    else 
-      while counter < (len - n) + 1
-        sub_arr = num_arr.slice(counter, n)
-        outer_arr << sub_arr
-        counter += 1
-      end
+    while counter < (@length - n) + 1
+      outer_arr << @number_array.slice(counter, n)
+      counter += 1
     end
     outer_arr
+  end
+
+  def to_num_array(string)
+    string.split('').map(&:to_i)
   end
 end
