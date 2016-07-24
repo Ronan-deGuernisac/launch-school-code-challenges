a19c89f671085ad33ca6672ba7e50a78029add43
superchilled <karl@superchilled.co.uk>
2016-07-24 17:49:14 +0100

    Refactoring following code review

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -2,36 +2,30 @@
 
 class Game # :nodoc:
   def initialize
-    @rolls = Array.new(24)
+    @rolls = []
     @frames = []
   end
 
   def roll(pins)
-    roll_index = next_empty
-    raise 'Pins must have a value from 0 to 10' unless (0..10).cover?(pins)
-    raise 'Pin count exceeds pins on the lane' if
-      open_rolls_over?(pins, roll_index)
-    raise 'Should not be able to roll after game is over' unless
-      game_in_progress?
-    @rolls[roll_index] = pins
-    @rolls[next_empty] = 'X' if pins == 10
+    validate_roll_value(pins)
+    validate_frame_value(pins)
+    validate_game_status
+
+    @rolls << pins
+    @rolls << 'X' if pins == 10
   end
 
   def score
-    raise 'Score cannot be taken until the end of the game' if game_in_progress?
-    raise 'Game is not yet over, cannot score!' if game_in_progress?
+    validate_standard_balls
+    validate_fill_balls
+
     @rolls.compact.each_slice(2) { |frame| @frames << frame }
-    frame_scores = @frames.map.with_index do |_, frame_index|
-      frame_score(frame_index)
+    @frames.each_with_index.reduce(0) do |sum, (_, frame_index)|
+      sum + frame_score(frame_index)
     end
-    frame_scores.flatten.reduce(:+)
   end
 
   private
-
-  def next_empty
-    @rolls.bsearch_index(&:nil?)
-  end
 
   def frame_score(frame_index)
     frame = @frames[frame_index]
@@ -60,23 +54,32 @@ class Game # :nodoc:
   end
 
   def spare?(frame)
-    frame.reduce(:+) == 10
+    !strike?(frame) && frame.reduce(:+) == 10
   end
 
   def fill_balls?(frame_index)
     frame_index == 10 || frame_index == 11
   end
 
-  def open_rolls_over?(pins, roll_index)
+  def open_rolls_over?(pins)
+    roll_index = @rolls.size
     roll_index.odd? && pins + @rolls[roll_index - 1] > 10
   end
 
   def game_in_progress?
-    @rolls.compact.size < 20 + fill_balls_spaces
+    standard_balls_not_played? || fill_balls_not_played?
+  end
+
+  def standard_balls_not_played?
+    @rolls.size < 20
+  end
+
+  def fill_balls_not_played?
+    @rolls.size < 20 + fill_balls_spaces
   end
 
   def fill_balls_spaces
-    last_frame = @rolls.slice(18, 2).map(&:to_i)
+    last_frame = @rolls.slice(18, 2)
     case
     when strike?(last_frame) && @rolls[20] == 10
       4
@@ -87,5 +90,28 @@ class Game # :nodoc:
     else
       0
     end
+  end
+
+  def validate_roll_value(pins)
+    raise 'Pins must have a value from 0 to 10' unless (0..10).cover?(pins)
+  end
+
+  def validate_frame_value(pins)
+    raise 'Pin count exceeds pins on the lane' if
+      open_rolls_over?(pins)
+  end
+
+  def validate_game_status
+    raise 'Should not be able to roll after game is over' unless
+      game_in_progress?
+  end
+
+  def validate_standard_balls
+    raise 'Score cannot be taken until the end of the game' if
+      standard_balls_not_played?
+  end
+
+  def validate_fill_balls
+    raise 'Game is not yet over, cannot score!' if fill_balls_not_played?
   end
 end
