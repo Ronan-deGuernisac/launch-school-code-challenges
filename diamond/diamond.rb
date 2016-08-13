a931854b96349dabc5f766fdbd6f535ec4704a62
superchilled <karl@superchilled.co.uk>
2016-08-13 10:33:47 +0100

    Extracting padding calc to DRY up code

--- diamond/diamond.rb
+++ diamond/diamond.rb
@@ -4,18 +4,26 @@ class Diamond #:nodoc:
   def self.make_diamond(transverse_letter)
     diamond = [*'A'..transverse_letter] + [*'A'...transverse_letter].reverse
     diamond.map { |letter| add_inner_padding(letter) }
-      .map { |section| add_outer_padding(section ,transverse_letter) }
-      .join("\n") + "\n"
+           .map { |section| add_outer_padding(section, transverse_letter) }
+           .join("\n") + "\n"
   end
 
   def self.add_inner_padding(letter)
-    padding = (letter.ord - 65) * 2
+    padding = calculate_padding(letter)
     return letter if padding == 0
     letter.ljust(padding) + letter
   end
 
   def self.add_outer_padding(section, transverse_letter)
-    width = (transverse_letter.ord - 65) * 2 + 1
+    width = calculate_padding(transverse_letter) + 1
     section.center(width)
+  end
+
+  class << self
+    private
+
+    def calculate_padding(letter)
+      (letter.ord - 65) * 2
+    end
   end
 end
