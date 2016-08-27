81c7f0848be8ac9fba95f9a9e976997440df4070
superchilled <karl@superchilled.co.uk>
2016-08-27 02:52:41 +0100

    Refactoring following rubocop

--- poker/poker.rb
+++ poker/poker.rb
@@ -13,71 +13,78 @@ require 'pry'
 #     High card = no other conditions met
 
 # In the event of hands of equal rank need to identify best hand of that rank
-#   Values of cards in each hand of the same rank are compared in order until one hand
-#   is found to have a card of higher value than another hand. If all cards are of equal value
+#   Values of cards in each hand of the same rank are compared in order
+#   until one hand
+#   is found to have a card of higher value than another hand. If all cards are
+#   of equal value
 #   then the round is tied (split pot)- both hands should be returned
 
-class Poker
+class Poker # :nodoc:
   def initialize(hands)
     @hands = []
     hands.each { |hand| @hands << PokerHand.new(hand) }
   end
 
   def best_hand
-    ranked_hands = rank_hands
-    # binding.pry
+    ranked_hands = rank_by_hand
     return sanitize_output(ranked_hands) if ranked_hands.size == 1
-    sanitize_output(rank_cards(ranked_hands))
+    sanitize_output(rank_by_card(ranked_hands))
   end
 
   def sanitize_output(hands)
     hands.map do |hand|
-      hand.cards.map do |card|
-        card.card_code
-      end
+      hand.cards.map(&:card_code)
     end
   end
 
-  def rank_hands
-    best_rank = @hands.map { |hand| hand.rank }.min
+  def rank_by_hand
+    best_rank = @hands.map(&:rank).min
     @hands.reject { |hand| hand.rank != best_rank }
   end
-  
-  def rank_cards(ranked_hands)
-    sorted_valued_hands = ranked_hands.map { |hand| hand.comparison_order }
-    
+
+  def rank_by_card(ranked_hands)
+    ordered_hands = ranked_hands.map(&:comparison_order)
+
     hands_ranked_by_card_values = nil
     5.times do
       hands_ranked_by_card_values = []
-      comparison_cards = {}
-      sorted_valued_hands.each_with_index do |hand, index|
-        comparison_cards[index] = hand.last
-        hand.rotate!
-      end
-      highest_value = comparison_cards.map { |card_key, card_value| card_value.value }.max
-      comparison_cards.reject! { |card_key, card_value| card_value.value < highest_value }
-      comparison_cards.keys.each { |hand_index| hands_ranked_by_card_values << ranked_hands[hand_index] }
+      compare_cards(ordered_hands, hands_ranked_by_card_values, ranked_hands)
       break if hands_ranked_by_card_values.size == 1
     end
     hands_ranked_by_card_values
   end
+
+  def compare_cards(ordered_hands, hands_ranked_by_card_values, ranked_hands)
+    comparison_cards = {}
+    ordered_hands.each_with_index do |hand, index|
+      comparison_cards[index] = hand.last
+      hand.rotate!
+    end
+    highest_value = comparison_cards
+                    .map { |_, card_value| card_value.value }.max
+    comparison_cards
+      .reject! { |_, card_value| card_value.value < highest_value }
+    comparison_cards.keys.each do |hand_index|
+      hands_ranked_by_card_values << ranked_hands[hand_index]
+    end
+  end
 end
 
-class PokerHand
+class PokerHand # :nodoc:
   attr_reader :cards, :comparison_order
 
   def initialize(hand)
     @cards = []
     hand.each { |card| @cards << Card.new(card) }
-    @hand_values = @cards.map { |card| card.value }
-    @hand_suits = @cards.map { |card| card.suit }
+    @hand_values = @cards.map(&:value)
+    @hand_suits = @cards.map(&:suit)
     @comparison_order = order_cards
   end
 
   def order_cards
-    ordered_cards = @cards.sort_by { |card| card.value }
+    ordered_cards = @cards.sort_by(&:value)
     if quads? && ordered_cards.first.value != ordered_cards.last.value
-      ordered_cards.rotate! 
+      ordered_cards.rotate!
     elsif full_house? && ordered_cards.first.value != ordered_cards.last.value
       ordered_cards.rotate!(2)
     end
@@ -86,15 +93,15 @@ class PokerHand
 
   def rank
     case
-      when straight_flush? then 1
-      when quads? then 2
-      when full_house? then 3
-      when flush? then 4
-      when straight? then 5
-      when trips? then 6
-      when two_pair? then 7
-      when pair? then 8
-      else 9
+    when straight_flush? then 1
+    when quads? then 2
+    when full_house? then 3
+    when flush? then 4
+    when straight? then 5
+    when trips? then 6
+    when two_pair? then 7
+    when pair? then 8
+    else 9
     end
   end
 
@@ -140,7 +147,7 @@ class PokerHand
   end
 end
 
-class Card
+class Card # :nodoc:
   CARD_VALUES = {
     '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
     '9' => 9, 'T' => 10, 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14
@@ -153,7 +160,7 @@ class Card
   end
 
   def value
-    @card_code.chars.first.gsub(/[2-9TJQKA]/,CARD_VALUES).to_i
+    @card_code.chars.first.gsub(/[2-9TJQKA]/, CARD_VALUES).to_i
   end
 
   def suit
