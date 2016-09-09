00e660e3e9a9fd507b03aecf01f971eb2e5b9f90
superchilled <karl@superchilled.co.uk>
2016-09-09 19:30:14 +0100

    DRYing and tidying

--- simple_cipher/simple_cipher.rb
+++ simple_cipher/simple_cipher.rb
@@ -1,8 +1,8 @@
 # simple_cipher.rb
 
-class Cipher
+class Cipher # :nodoc:
   ALPHABET = [*'a'..'z'].freeze
-  DEFAULT_KEY = "dddddddddddddddddddddddddddddddddddddddd".freeze
+  DEFAULT_KEY = 'dddddddddddddddddddddddddddddddddddddddd'.freeze
 
   attr_reader :key
 
@@ -12,25 +12,11 @@ class Cipher
   end
 
   def encode(string)
-    characters = string.chars
-    characters.map.with_index do |character, index|
-      key_char = @key[index]
-      distance_to_key_char = ALPHABET.index(key_char) - ALPHABET.index('a')
-      char_alpha_index = ALPHABET.index(character)
-      rotated_alpha = ALPHABET.rotate(distance_to_key_char)
-      rotated_alpha[char_alpha_index]
-    end.join
+    cipher(string).join
   end
 
   def decode(string)
-    characters = string.chars
-    characters.map.with_index do |character, index|
-      key_char = @key[index]
-      distance_to_key_char = ALPHABET.index(key_char) - ALPHABET.index('a')
-      char_alpha_index = ALPHABET.index(character)
-      rotated_alpha = ALPHABET.rotate(- distance_to_key_char)
-      rotated_alpha[char_alpha_index]
-    end.join
+    cipher(string, true).join
   end
 
   private
@@ -38,5 +24,15 @@ class Cipher
   def check_for_errors(key)
     raise ArgumentError if key == ''
     raise ArgumentError if key =~ /[A-Z0-9]/
+  end
+
+  def cipher(string, reverse = false)
+    string.chars.map.with_index do |character, index|
+      key_letter = @key[index]
+      distance_to_key_letter = ALPHABET.index(key_letter) - ALPHABET.index('a')
+      distance_to_key_letter = - distance_to_key_letter if reverse
+      rotated_alphabet = ALPHABET.rotate(distance_to_key_letter)
+      rotated_alphabet[ALPHABET.index(character)]
+    end
   end
 end
