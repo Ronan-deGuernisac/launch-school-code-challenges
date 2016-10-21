55406bba90adfa05e2854eacf2720b749d93e896
superchilled <karl@superchilled.co.uk>
2016-10-21 23:57:18 +0100

    Refactoring for readability and following rubocop

--- wordy/wordy.rb
+++ wordy/wordy.rb
@@ -1,18 +1,4 @@
 # wordy.rb
-require 'pry'
-# - WordProblem class takes a string as an argument to initialize method
-#   - The string comprises of:
-#     - Integers in String format `'1'`, `'2'`, etc..
-#     - Operations words in String format `'plus'`, `'minus'`, `'multiplied by'`, `'divided by'`
-#       - Operations such as `'cubed'` are not valid
-#     - A string should have at least two numbers and one valid operation to be a valid WordProblem
-#       - An invalid WordProblem should return an ArgumentError
-#     - Operations are carried out sequentially (i.e. normal operation order is not respected)
-# - WordProblem class has a `answer` instance method which returns an integer value of the answer
-
-# - Need to replace the number strings in the string with integers and the 
-# operations strings with operations
-
 
 class WordProblem # :nodoc:
   OPERATIONS = {
@@ -20,7 +6,7 @@ class WordProblem # :nodoc:
     'minus' => '-',
     'multiplied' => '*',
     'divided' => '/'
-  }
+  }.freeze
 
   def initialize(question)
     @question = question
@@ -29,9 +15,10 @@ class WordProblem # :nodoc:
   end
 
   def answer
-    total = @maths_problem.shift
-    until @maths_problem.empty?
-      total = total.send @maths_problem.shift, @maths_problem.shift
+    total = next_element
+    loop do
+      total = total.send(next_element, next_element)
+      break if @maths_problem.empty?
     end
     total
   end
@@ -44,16 +31,35 @@ class WordProblem # :nodoc:
         OPERATIONS[word]
       elsif word =~ /\d/
         word.to_i
-      else
-        nil 
-      end 
+      end
     end
   end
 
-  def maths_problem_valid?
-    @maths_problem.size >= 3 && 
-    @maths_problem.select.with_index { |item, index| index.even? }.all? { |item| item.is_a? Integer } &&
-    @maths_problem.select.with_index { |item, index| index.odd? }.all? { |item| OPERATIONS.values.include?(item) }
+  def next_element
+    @maths_problem.shift
   end
 
+  def maths_problem_valid?
+    sufficient_length? && even_items_integers? && odd_items_operations?
+  end
+
+  def sufficient_length?
+    @maths_problem.size >= 3
+  end
+
+  def even_items_integers?
+    even_items.all? { |item| item.is_a? Integer }
+  end
+
+  def odd_items_operations?
+    odd_items.all? { |item| OPERATIONS.values.include?(item) }
+  end
+
+  def even_items
+    @maths_problem.select.with_index { |_, index| index.even? }
+  end
+
+  def odd_items
+    @maths_problem.select.with_index { |_, index| index.odd? }
+  end
 end
